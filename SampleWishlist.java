7d4619761780e4778a8ebbc681d95afbbb8f8ad2
Ubaidillah <ubaidillah.jobs@gmail.com>
2022-08-23 16:29:49 +0700

    PostTest 18

---
+++ SampleWishlist.java
@@ -0,0 +1,82 @@
+package com.juara.coding.PageObject.PostTest18;
+
+
+import java.util.List;
+
+import javax.swing.MenuElement;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.juaracoding.pageObject.drivers.DriverSingleton;
+
+	public class SampleWishlist {
+		
+		private WebDriver driver;
+
+			public SampleWishlist() {
+				this.driver = DriverSingleton.getDriver();
+				PageFactory.initElements(driver, this);
+			}
+			//input[@id='search_query_top']
+			@FindBy(xpath = "//input[@id='search_query_top']")
+			private WebElement btnTxtSearch;
+			
+			@FindBy(xpath = "//input[@id='search_query_top']")
+			private WebElement txtSearch;
+			
+			@FindBy(xpath = "//button[@name='submit_search']")
+			private WebElement btnSearch;
+			
+			@FindBy(xpath = "//li[@class='ajax_block_product col-xs-12 col-sm-6 col-md-4 first-in-line last-line first-item-of-tablet-line first-item-of-mobile-line last-mobile-line hovered']//span[contains(text(),'Add to cart')]")
+			private WebElement btnChart1;
+			
+			@FindBy(xpath = "//span[@title='Close window']")
+			private WebElement btnCloseChart1;
+			
+			@FindBy(xpath = "//li[@class='ajax_block_product col-xs-12 col-sm-6 col-md-4 last-line last-item-of-tablet-line last-mobile-line hovered']//span[contains(text(),'Add to cart')]")
+			private WebElement btnChart2;
+			
+			@FindBy(xpath = "//span[@title='Close window']")
+			private WebElement btnCloseChart2;
+			
+			
+			public void btnTxtSearch() {
+				btnTxtSearch.click();
+			}
+			public void txtSearch() {
+				this.txtSearch.sendKeys("Printed Summer Dress");
+			}
+			
+			public void btnSearch() {
+				btnSearch.click();
+			}
+			
+			public void btnChart1() {
+				btnChart1.click();
+			}
+			
+			public void btnCloseChart1() {
+				btnCloseChart1.click();
+			}
+			
+			public void btnChart2() {
+				btnChart2.click();
+			}
+			
+			public void btnCloserChart2() {
+				btnCloseChart2.click();
+			}
+			
+//			public void uploadGambar() {
+//				uploadFile.sendKeys("D:\\myAvatar.png");
+//			}
+//			
+//			public void pilihMenuElement() {
+//				menuElement.click();
+//				listMenuElement.get(0).click();
+//				System.out.println(listMenuElement.get(0).getText());
+//			}
+		}
